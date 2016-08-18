792805402ed5fdd1de3ebc79cf2da4e32b5b1d01
Jason Noble <github+jasonn@jasonnoble.org>
2016-08-17 20:55:21 -0600

    Added theme

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'cars#index'
+  root to: 'static_pages#index'
 
   get 'about' => 'static_pages#about'
+  get 'login' => 'static_pages#login'
 end
